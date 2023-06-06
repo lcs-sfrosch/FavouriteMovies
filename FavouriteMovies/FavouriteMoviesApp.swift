75f291032dbdf50fd6b413e99fa63e379b9c3e4b
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-06-06 11:15:15 -0400

    Initial Commit

---
+++ FavouriteMovies/FavouriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteMoviesApp.swift
+//  FavouriteMovies
+//
+//  Created by Sebastian Frosch on 06.06.23.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
